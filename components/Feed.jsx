6cf214b5dce73de1c607f6dba59cc02f88d0a179
Sheldon Liang <lquanzhao@gmail.com>
2024-03-21 22:11:53 -0700

    Added Search bar in Home Page

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+'use client';
+import Image from 'next/image';
+import { useState, useEffect } from 'react';
+import PromptCard from './PromptCard';
 
 const Feed = () => {
+
+  const [searchText, setSearchText] = useState('');
+
+  const handleSearchChange = (e) => {
+
+  }
+
+
   return (
-    <div>Feed</div>
+    <section className='feed'>
+      <form className='relative w-full flex-center'>
+        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+          <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+          </svg>
+        </div>
+        <input
+          className='search_input peer ml-2'
+          type='text'
+          placeholder='Search for a tag or a username'
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+        />
+      </form>
+    </section>
   )
 }
 
